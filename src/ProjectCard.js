c9e399c0bc04b3cbabbd4f108baf6a1b88eb48e7
Charles Henville <henvillecharles@gmail.com>
2023-05-09 03:51:18 -0400

    Format Imprv for Projects

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -4,16 +4,41 @@ import ProjectMedia from './ProjectMedia';
 
 const ProjectCard = ({ items }) => {
 
+    const spotItem = items.find(item => item.id === 0);
+
     return (
-        <div>
-            {items.map((item) => (
-                <div className={styles.cardMain}>
-                    <ProjectMedia videoUrl={item.vid} thumbnailUrl={item.thumb}/>
-                    <p>{item.name}</p>
-                    <p>{item.date}</p>
+
+        <div className={styles.mainWidth}>
+
+            <div className={styles.cardSpotPadding}>
+                <div className={styles.cardSpotMain}>
+                    <ProjectMedia isSpot={true} videoUrl={spotItem.vid} thumbnailUrl={spotItem.thumb} />
+                    <div className={styles.spotInfo}>
+                        <h3>{spotItem.name}</h3>
+                        <div> </div>
+                        <p>{spotItem.date}</p>
+                    </div>
                 </div>
-            ))}
+            </div>
+
+            <div className={styles.cardWrapper}>
+                {items.map((item) => (
+                    (item.id) == 0 ? (null) :
+                        (
+                            <div className={styles.cardPadding}>
+                                <div className={styles.cardMain}>
+                                    <ProjectMedia isSpot={false} videoUrl={item.vid} thumbnailUrl={item.thumb} />
+                                    <h3>{item.name}</h3>
+                                    <p>{item.date}</p>
+                                </div>
+                            </div>
+                        )
+
+                ))}
+            </div>
+
         </div>
+
     );
 };
 
--- src/ProjectMedia.js
+++ src/ProjectMedia.js
@@ -1,25 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import styles from './projects.module.css'
 
-const ProjectMedia = ({ videoUrl, thumbnailUrl }) => {
-  const [isHovering, setIsHovering] = useState(false);
+const ProjectMedia = ({ isSpot, videoUrl, thumbnailUrl }) => {
+    const [isHovering, setIsHovering] = useState(false);
+    const videoRef = useRef(null);
 
-  const handleMouseEnter = () => {
-    setIsHovering(true);
-  };
+    const handleMouseEnter = () => {
+        setIsHovering(true);
+        videoRef.current.play();
+    };
 
-  const handleMouseLeave = () => {
-    setIsHovering(false);
-  };
+    const handleMouseLeave = () => {
+        setIsHovering(false);
+        videoRef.current.pause();
+        //videoRef.current.currentTime = 0;
+    };
 
-  return (
-    <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      {isHovering ? (
-        <video src={videoUrl} autoPlay muted loop />
-      ) : (
-        <img src={thumbnailUrl} alt="Thumbnail" />
-      )}
-    </div>
-  );
+    const vidStyle = {
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
+        objectFit: 'cover',
+        opacity: isHovering ? 1 : 0,
+        transition: 'opacity 0.3s ease-in-out',
+        zIndex: 2,
+        borderRadius: isSpot ? ('10px 10px 10px 10px') : ('10px 10px 0px 0px'),
+        top: '0',
+        left: '0'
+    };
+
+    return (
+        <div className={styles.mediaContainer} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <video style={vidStyle} src={videoUrl} muted loop ref={videoRef} />
+            <img style={{borderRadius : isSpot ? ('10px 10px 10px 10px') : ('10px 10px 0px 0px')}} className={styles.media} src={thumbnailUrl} alt="Thumbnail" />
+        </div>
+    );
 };
 
 export default ProjectMedia;
