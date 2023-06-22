4e6d61fcd714da4f0a2ca29d8ffc2417231d545f
Charles Henville <98337598+charleshenville@users.noreply.github.com>
2023-06-22 15:05:02 -0400

    Update App.js Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import MenuBar from './components/MenuBar';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
@@ -13,10 +13,12 @@ function App() {
     <div className="App">
       <MenuBar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/resume' element={<Resume />} />
-        <Route path='/contact' element={<Contact />} />
+        <Switch>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/projects' exact element={<Projects />} />
+          <Route path='/resume' exact element={<Resume />} />
+          <Route path='/contact' exact element={<Contact />} />
+        </Switch>
       </Routes>
     </div>
 
